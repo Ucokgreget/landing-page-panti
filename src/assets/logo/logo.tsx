38e56419aeab94095e99a9a2bdd6f9e478b7cd46
Ucokgreget <luqmanalfauzan1738@gmail.com>
2026-07-06 01:12:28 +0700

    fix:logo kebesaran

--- src/assets/logo/logo.tsx
+++ src/assets/logo/logo.tsx
@@ -5,7 +5,7 @@ import logo from "../../../public/logo.png"
 const Logo = (props: SVGAttributes<SVGElement>) => {
   return (
     <div className="flex items-center gap-2.5">
-        <Image src={logo} alt="Logo" width={140} height={40} loading="eager" priority style={{ width: 'auto', height: 'auto' }} />
+        <Image src={logo} alt="Logo" width={280} height={80} loading="eager" priority className="h-12 w-auto" />
       </div>
   );
 };
